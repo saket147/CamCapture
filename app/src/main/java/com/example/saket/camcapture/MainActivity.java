5d36a74da0f2c3b6428c28967532b1ec41dced33
saket <saket19954@gmail.com>
2016-08-30 02:36:09 +0530

    Added permission for M

--- app/src/main/java/com/example/saket/camcapture/MainActivity.java
+++ app/src/main/java/com/example/saket/camcapture/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.saket.camcapture;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import java.io.File;
+import java.util.jar.Manifest;
 
 
 public class MainActivity extends AppCompatActivity
@@ -20,6 +25,9 @@ public class MainActivity extends AppCompatActivity
     Button btnCamera;
     private static final int PICK_FROM_CAMERA = 0;
     private static final int VIDEO_CAPTURE = 102;
+    private int STORAGE_PERMISSION_CODE = 23;
+    private int RECORD_AUDIO_CODE = 23;
+    private int CAPTURE_VIDEO_OUTPUT = 23;
 
 
 
@@ -32,39 +40,149 @@ public class MainActivity extends AppCompatActivity
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File imagesFolder = new File(Environment.getExternalStorageDirectory(), "MyImages");
-                imagesFolder.mkdirs(); // <----
-                File image = new File(imagesFolder, "image_001.jpg");
-                Uri uriSavedImage = Uri.fromFile(image);
-                imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
-                imageIntent.putExtra(MediaStore.EXTRA_SIZE_LIMIT,1098304L);
-                startActivityForResult(imageIntent,PICK_FROM_CAMERA);
+
+                if(isWriteStorageAllowed()) {
+
+
+                    Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File imagesFolder = new File(Environment.getExternalStorageDirectory(), "CamCapture");
+                    imagesFolder.mkdirs(); // <----
+                    File image = new File(imagesFolder, "image_001.jpg");
+                    Uri uriSavedImage = Uri.fromFile(image);
+                    imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+                    imageIntent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, 0);
+                    imageIntent.putExtra(MediaStore.EXTRA_SIZE_LIMIT, 1098304L);
+                    startActivityForResult(imageIntent, PICK_FROM_CAMERA);
+
+                }
+                requestStoragePermission();
+
+                /*imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+                imageIntent.putExtra("crop", "true");
+                imageIntent.putExtra("outputX", 150);
+                imageIntent.putExtra("outputY", 150);
+                imageIntent.putExtra("aspectX", 1);
+                imageIntent.putExtra("aspectY", 1);
+                imageIntent.putExtra("scale", true);
+                imageIntent.putExtra("outputFormat",
+                        Bitmap.CompressFormat.JPEG.toString());*/
+                //startActivityForResult(imageIntent,PICK_FROM_CAMERA);
+
             }
         });
 
         btnCamera.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                File mediaFile =
-                        new File(Environment.getExternalStorageDirectory().getAbsolutePath()
-                                + "/myvideo.mp4");
+                if(isWriteStorageAllowed()||isCaptureVideoOutput()||isRecordAudioCode()) {
+                    File mediaFile =
+                            new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                                    + "/myvideo.mp4");
 
-                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                    Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
 
-                Uri videoUri = Uri.fromFile(mediaFile);
+                    Uri videoUri = Uri.fromFile(mediaFile);
 
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, videoUri);
-                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY,0);
-                intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT,30);
-                intent.putExtra(MediaStore.EXTRA_SIZE_LIMIT, 10380902L);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, videoUri);
+                    intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+                    intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 30);
+                    intent.putExtra(MediaStore.EXTRA_SIZE_LIMIT, 10380902L);
 
-                startActivityForResult(intent,VIDEO_CAPTURE);
+                    startActivityForResult(intent, VIDEO_CAPTURE);
+                }
+                requestStoragePermission();
+                requestCaptureVideoOutput();
+                requestRecordAudio();
 
-                return false;
+                return true;
             }
         });
     }
+    private boolean isWriteStorageAllowed() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        return false;
+    }
+
+    private boolean isRecordAudioCode() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO);
+
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        return false;
+    }
+    private boolean isCaptureVideoOutput() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAPTURE_VIDEO_OUTPUT);
+
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        return false;
+    }
+
+    private void requestStoragePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+
+        }
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                STORAGE_PERMISSION_CODE);
+    }
+    private void requestRecordAudio(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.RECORD_AUDIO)){
+
+        }
+
+
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.RECORD_AUDIO},
+                STORAGE_PERMISSION_CODE);
+    }
+    private void requestCaptureVideoOutput(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CAPTURE_VIDEO_OUTPUT)){
+
+        }
+
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.CAPTURE_VIDEO_OUTPUT},
+                STORAGE_PERMISSION_CODE);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == STORAGE_PERMISSION_CODE){
+
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                Toast.makeText(this,"Permission granted now you can read the storage",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"Oops you just denied the permission",Toast.LENGTH_LONG).show();
+            }
+        }
+        if(requestCode == CAPTURE_VIDEO_OUTPUT){
+
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                Toast.makeText(this,"Permission granted now you can capture video",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"Oops you just denied the permission",Toast.LENGTH_LONG).show();
+            }
+        }
+        if(requestCode == RECORD_AUDIO_CODE){
+
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                Toast.makeText(this,"Permission granted now you can record audio",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"Oops you just denied the permission",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == VIDEO_CAPTURE) {
             if (resultCode == RESULT_OK) {
